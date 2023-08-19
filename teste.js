9d955b35697842a2b86418a1f8fc34c2bbb675b3
fregnani123 <fabianofregnani@hotmail.com>
2023-08-18 21:16:27 -0300

    estudos

--- teste.js
+++ teste.js
@@ -2,8 +2,21 @@
 
 class validaCPF{
     constructor(cpfEnviado) {
-        this.cpfEnviado = cpfEnviado
+
+        Object.defineProperty(this, 'cpfLimpo', {
+            value: cpfEnviado.replace(/\D+/g,''),
+            writable: 'false',
+            enumerable:'false',
+      })
     }
+    isValid() {
+        if (!this.cpfLimpo) return false;
+        if (this.cpfLimpo !== 'string') return false;
+   }
+   
 }
 
 const verificaCPF = new validaCPF('063.912.989-71')
+
+console.log(verificaCPF.cpfLimpo
+)
